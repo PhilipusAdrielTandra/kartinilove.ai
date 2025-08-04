eea65176a96362376ce941739923d34e257fe35a
PhilipusAdrielTandra <philipusadriel@gmail.com>
2025-08-04 20:25:57 +0700

    Updated frontend, new hero

--- src/App.tsx
+++ src/App.tsx
@@ -11,26 +11,26 @@ export default function App() {
       <div className="space-x-4">
         <Link
           to="/"
-          className="relative text-gray-700 hover:text-pink-600 after:content-[''] after:absolute after:left-0 after:-bottom-1 after:w-0 after:h-[2px] after:bg-pink-600 after:transition-all after:duration-300 hover:after:w-full"
+          className="relative hover:text-pink-600 after:content-[''] after:absolute after:left-0 after:-bottom-1 after:w-0 after:h-[2px] after:bg-pink-600 after:transition-all after:duration-300 hover:after:w-full"
         >
           Home
         </Link>
         <Link
           to="/"
-          className="relative text-gray-700 hover:text-pink-600 after:content-[''] after:absolute after:left-0 after:-bottom-1 after:w-0 after:h-[2px] after:bg-pink-600 after:transition-all after:duration-300 hover:after:w-full"
+          className="relative hover:text-pink-600 after:content-[''] after:absolute after:left-0 after:-bottom-1 after:w-0 after:h-[2px] after:bg-pink-600 after:transition-all after:duration-300 hover:after:w-full"
         >
           Blog
         </Link>
         <Link
           to="/"
-          className="relative text-gray-700 hover:text-pink-600 after:content-[''] after:absolute after:left-0 after:-bottom-1 after:w-0 after:h-[2px] after:bg-pink-600 after:transition-all after:duration-300 hover:after:w-full"
+          className="relative hover:text-pink-600 after:content-[''] after:absolute after:left-0 after:-bottom-1 after:w-0 after:h-[2px] after:bg-pink-600 after:transition-all after:duration-300 hover:after:w-full"
         >
           About Us
         </Link>
       </div>
       <div className="space-x-4">
           <button className="border-solid border-2 rounded-md px-2 py-1"><Link to="/language" className="text-black hover:text-gray-200">EN</Link></button>
-          <button className="bg-pink-600 rounded-full px-4 py-4"><Link to="/chat" className="text-white hover:text-gray-200">Explore Now</Link></button>
+          <button className="bg-pink-600 rounded-full px-4 py-3"><Link to="/chat" className="text-white hover:text-gray-200">Explore Now</Link></button>
         </div>
       </div>
 
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,32 @@
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Hero from "../assets/hero.png"
 export default function Home() {
   return (
-    <div className="min-h-screen flex flex-col pt-20 items-center  bg-gray-50">
-      <h1 className="text-4xl font-bold mb-4">Lorem ipsum dolor sit amet</h1>
-      <p className="text-lg text-gray-600">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+    <div className="overflow-x-hidden"> 
+      <div className="relative min-h-screen">
+        <img
+          src={Hero}
+          className="w-6xl absolute -left-180 -top-44 z-20"
+        />
+        <img
+          src={Hero}
+          className="w-7xl absolute -bottom-34 -right-44 z-20"
+        />
+        <div className="min-h-screen flex flex-col pt-20 items-center bg-linear-to-b to-[#FFE7EA] from-pink-400 overflow-hidden relative z-10">
+          <h1 className="text-6xl mb-4 w-2xl text-center">Lorem Ipsum Dolor Sit Amet</h1>
+          <p className="text-md text-gray-600 text-center my-2 w-96 h-auto">
+            Lorem ipsum dolor sit amet, consuectur adipiscing elit, sed o elusmad tempor incidunt ut labore et dolore magna aliqua.
+          </p>
+          <button className="bg-pink-600 rounded-full px-5 my-5 py-3">
+            <Link to="/chat" className="text-white hover:text-gray-200">
+              Explore Now
+            </Link>
+          </button>
+        </div>
+      </div>
+      <div className="min-h-screen flex flex-col items-center bg-[#FFE7EA] relative z-0">
+        <h2 className="text-4xl mb-4">Get to Know Us</h2>
+      </div>
     </div>
   );
 }
