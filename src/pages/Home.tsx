87beab43dcb75214d94cabc6ec83997efaec9b80
PhilipusAdrielTandra <philipusadriel@gmail.com>
2025-08-07 14:40:06 +0700

    Full frontend done for homepage, need to work on mobile responsiveness next and the chat ui

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,7 @@
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Hero from "../assets/hero.png";
+import Star from "../assets/star.png";
+import Plus from "../assets/plus.png"
 
 export default function Home() {
   return (
@@ -20,7 +22,7 @@ export default function Home() {
           <p className="text-md text-gray-600 text-center my-2 w-96 h-auto">
             Lorem ipsum dolor sit amet, consuectur adipiscing elit, sed o elusmad tempor incidunt ut labore et dolore magna aliqua.
           </p>
-          <button className="bg-[#EF0753] rounded-full px-7 my-5 py-3">
+          <button className="bg-[#EF0753] rounded-full px-7 py-3  my-5">
             <Link to="/chat" className="text-white hover:text-gray-200">
               Explore Now
             </Link>
@@ -36,7 +38,7 @@ export default function Home() {
             width="1260"
             height="550"
             className="rounded-2xl"
-            src="https://www.youtube.com/embed/LIlZCmETvsY?si=Gg0hA3kbqWMj0MZN"
+            src="https://www.youtube.com/embed/PrttntkPgyg?si=knt9C8EXLj6Wi4SI"
             title="YouTube video player"
             frameBorder="0"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
@@ -45,12 +47,138 @@ export default function Home() {
           ></iframe>
         </div>
       </div>
-      <div className="min-h-screen flex flex-col items-center bg-[#FFE7EA] relative">
-        <h2 className="text-5xl mt-24 relative z-20 w-96 text-center">
+      <div className="min-h-screen flex flex-col items-center bg-[#FFE7EA] relative text-center">
+        <h2 className="text-5xl mt-24 mb-10 relative z-20 w-96">
           Key Benefit of <a className="text-[#EF0753]">Our Services</a>
         </h2>
-        <div className="">
+        <div className="bg-white px-8 py-5 rounded-2xl shadow">
+          <img src={Star} className="w-30 text-center mx-auto -mb-4"></img>
+          <h1 className="text-3xl my-4">Lorem Ipsum</h1>
+          <p className="w-72">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi sed pulvinar leo. Donec in orci iaculis, lacinia nulla at, pretium neque.</p>
+        </div>
+      </div>
+      <div className="min-h-screen flex  bg-[#FFE7EA] text-center">
+        <div className="flex w-full max-w-screen">
+          {/* Left Side */}
+          <div className="w-1/2 flex items-start">
+            <div className="p-8">
+            </div>
+          </div>
 
+          {/* Right Side */}
+          <div className="w-1/2 flex items-start">
+            <div className="p-8">
+              <h2 className="text-5xl mb-4 text-left">How It Works</h2>
+              <div className="flex items-center space-x-4 mb-4 my-12">
+                <div className="text-5xl  text-[#feb0b9] mr-8">01</div>
+                  <div className="relative z-10 gradient-shadow-wrapper">
+                    <div className="bg-white px-15 py-4 rounded-2xl shadow-md relative z-20">
+                      <h1 className="text-xl text-left text-gray-800">Lorem Ipsum Sit Amet</h1>
+                      <p>Lorem ipsum dolor sit amet, consectetur</p>
+                    </div>
+                  </div>
+              </div>
+              <div className="flex flex-row-reverse items-center space-x-4 space-x-reverse mb-4 my-12">
+                <div className="text-5xl text-[#feb0b9] mr-8">02</div>
+                <div className="relative z-10 gradient-shadow-wrapper">
+                  <div className="bg-white px-15 py-4 rounded-2xl shadow-md relative z-20">
+                    <h1 className="text-xl text-left text-gray-800">Lorem Ipsum Sit Amet</h1>
+                    <p>Lorem ipsum dolor sit amet, consectetur</p>
+                  </div>
+                </div>
+              </div>
+              <div className="flex items-center space-x-4 mb-4 my-12">
+                <div className="text-5xl  text-[#feb0b9] mr-8">03</div>
+                  <div className="relative z-10 gradient-shadow-wrapper">
+                    <div className="bg-white px-15 py-4 rounded-2xl shadow-md relative z-20">
+                      <h1 className="text-xl text-left text-gray-800">Lorem Ipsum Sit Amet</h1>
+                      <p>Lorem ipsum dolor sit amet, consectetur</p>
+                    </div>
+                  </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="pb-48 flex flex-col items-center  bg-[#FFE7EA] text-center px-4">
+        <h1 className="text-5xl mb-12">Collaboration & Partnership</h1>
+
+        <div className="flex flex-wrap justify-center gap-15 max-w-4xl">
+          {Array.from({ length: 9 }).map((_, index) => (
+            <div
+              key={index}
+              className="w-10 h-10 rounded-full bg-gray-300"
+            ></div>
+          ))}
+        </div>
+      </div>
+      <div className="min-h-screen flex flex-col bg-[#FFE7EA] text-center px-4 py-16">
+        <div className="w-full max-w-6xl mx-auto">
+          <h1 className="text-5xl mb-12 text-left">Frequently Asked Questions</h1>
+          <div className="flex gap-8 items-start">
+            {/* Left Column */}
+            <div className="flex flex-col flex-1 gap-6">
+              <div className="bg-white rounded-2xl shadow p-6 text-left relative">
+                <img src={Plus} className="absolute top-4 right-4 w-5 h-5" />
+                <div className="flex items-start gap-4">
+                  <div className="w-12 h-12 bg-[#EDEEF0] text-[#EF0753] text-md rounded-full flex items-center justify-center">
+                    01
+                  </div>
+                  <p className="text-xl max-w-xs">
+                    Lore ipsum sit amet consectetur adipiscing elit
+                  </p>
+                </div>
+              </div>
+              <div className="bg-white rounded-2xl shadow p-6 text-left relative">
+                <img src={Plus} className="absolute top-4 right-4 w-5 h-5" />
+                <div className="flex items-start gap-4">
+                  <div className="w-12 h-12 bg-[#EDEEF0] text-[#EF0753] text-md rounded-full flex items-center justify-center">
+                    02
+                  </div>
+                  <p className="text-xl max-w-xs">
+                    Lore ipsum sit amet consectetur adipiscing elit
+                  </p>
+                </div>
+              </div>              
+              <div className="bg-white rounded-2xl shadow p-6 text-left relative">
+                <img src={Plus} className="absolute top-4 right-4 w-5 h-5" />
+                <div className="flex items-start gap-4">
+                  <div className="w-12 h-12 bg-[#EDEEF0] text-[#EF0753] text-md rounded-full flex items-center justify-center">
+                    03
+                  </div>
+                  <p className="text-xl max-w-xs">
+                    Lore ipsum sit amet consectetur adipiscing elit
+                  </p>
+                </div>
+              </div>
+            </div>
+
+            {/* Right Column */}
+            <div className="flex flex-col flex-1 gap-6">
+              <div className="bg-white rounded-2xl shadow p-6 text-left relative">
+                <img src={Plus} className="absolute top-4 right-4 w-5 h-5" />
+                <div className="flex items-start gap-4">
+                  <div className="w-12 h-12 bg-[#EDEEF0] text-[#EF0753] text-md rounded-full flex items-center justify-center">
+                    04
+                  </div>
+                  <p className="text-xl max-w-xs">
+                    Lore ipsum sit amet consectetur adipiscing elit
+                  </p>
+                </div>
+              </div>
+              <div className="bg-white rounded-2xl shadow p-6 text-left relative">
+                <img src={Plus} className="absolute top-4 right-4 w-5 h-5" />
+                <div className="flex items-start gap-4">
+                  <div className="w-12 h-12 bg-[#EDEEF0] text-[#EF0753] text-md rounded-full flex items-center justify-center">
+                    05
+                  </div>
+                  <p className="text-xl max-w-xs">
+                    Lore ipsum sit amet consectetur adipiscing elit
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
