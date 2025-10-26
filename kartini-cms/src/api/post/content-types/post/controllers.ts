7177161895726c344b638f26b4c79a5ec9d6ca21
Joe19110 <joelliane@gmail.com>
2025-10-26 22:30:06 +0700

    strapi-connection-trial-2

---
+++ kartini-cms/src/api/post/content-types/post/controllers.ts
@@ -0,0 +1,4 @@
+import factories from '@strapi/strapi/dist/core-api';
+
+export default factories.createCoreController('api::post.post');
+
---
+++ kartini-cms/src/api/post/content-types/post/lifecycles.ts
@@ -0,0 +1,4 @@
+export default {
+  // Lifecycle functions (can be empty for now)
+};
+
